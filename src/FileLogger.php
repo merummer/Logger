c6b413fa14391d092417a9e9c1c05505f7fe0495
Melissa Maria Rummer <mrummer@schueler.hakzell.at>
2023-03-20 21:24:04 +0100

    create file logger

---
+++ src/FileLogger.php
@@ -0,0 +1,19 @@
+<?php
+namespace Merummer\Logger;
+
+class FileLogger implements ILogger
+{
+    private $handle = null;
+    public function __construct(string $filename){
+        $this->handle = fopen($filename,'a');
+}
+    public function logEntry(string $line): void
+    {
+        fwrite($this->handle, $line . "\n");
+    }
+
+    public function __destruct()
+    {
+        fclose($this->handle);
+    }
+}
